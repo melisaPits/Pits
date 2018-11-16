a3727eb69b19c3fe095b4cb2a1aaccfd996352e6
melisaPits <hdzs.melisa@gmail.com>
2018-11-16 15:02:21 -0600

    actualizacion wicked_pdf_image en tag_image3

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -13,8 +13,8 @@ WickedPdf.config = {
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-   :exe_path => 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+   exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  # :exe_path => 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
